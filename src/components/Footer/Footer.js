900a1b59c8af1a0d83cc4ba08ada466012e64515
Dexuan Song <dson459@outlook.com>
2022-05-27 00:31:49 +1200

    fix minor issure

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
         <div className="footerwrapper">
           <div className="footerleft">©2022 Turners</div>
           <div className="footerright">
-            <a href="">Branch Details</a>
-            <a href="">Facebook</a>
-            <a href="">Newsletter</a>
-            <a href="">Email Alerts</a>
-            <a href="">Instagram</a>
+            <a href="/#">Branch Details</a>
+            <a href="/#">Facebook</a>
+            <a href="/#">Newsletter</a>
+            <a href="/#">Email Alerts</a>
+            <a href="/#">Instagram</a>
           </div>
         </div>
       </div>
--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -18,7 +18,7 @@ const validationSchema = yup.object({
     // .min(8, "Password should be of minimum 8 characters length")
     .required("Password is required")
     .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
       "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
     ),
 });
